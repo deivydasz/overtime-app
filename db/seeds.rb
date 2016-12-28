33199632fec5637601fde43e6febda27a1e8ae6d
ecuSinner <deivydas.ziziliauskas@gmail.com>
2016-12-28 13:11:55 +0200

    Built out rake tasks for sending sms messages and STI for employees

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,24 @@
-@user = User.create(email: "test@test.com", 
+@employee = Employee.create(email: "test@test.com", 
 										password: "asdfasdf", 
 										password_confirmation: "asdfasdf", 
 										first_name: "Jon", 
 										last_name: "Snow",
-										phone: "8867049510")
+										phone: "860935011")
 
-puts "1 User created"
+puts "1 Employee created"
 
 AdminUser.create(email: "admintest@test.com", 
 								password: "asdfasdf", 
 								password_confirmation: "asdfasdf", 
 								first_name: "Admin", 
 								last_name: "Adam",
-								phone: "8867049510")
+								phone: "860935011")
 
 puts "1 Admin User created"
 
-	AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
-	AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 13.days))
-	AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 20.days))
+	AuditLog.create!(user_id: @employee.id, status: 0, start_date: (Date.today - 6.days))
+	AuditLog.create!(user_id: @employee.id, status: 0, start_date: (Date.today - 13.days))
+	AuditLog.create!(user_id: @employee.id, status: 0, start_date: (Date.today - 20.days))
 # end
 puts "3 Audit logs have been created"
 
@@ -26,7 +26,7 @@ puts "3 Audit logs have been created"
 	Post.create!(date: Date.today, rationale: "#{post} Tote bag hot chicken echo park irony mumblecore blue bottle
 	 raw denim prism slow-carb. Pinterest ennui trust fund drinking vinegar, normcore tumeric whatever jianbing kogi 
 	 activated charcoal. Celiac gochujang bicycle rights cardigan, everyday carry umami ethical stumptown migas helvetica 
-	 pinterest iPhone yuccie. Literally viral cardigan enamel pin whatever", user_id: @user.id, overtime_request: 2.5)
+	 pinterest iPhone yuccie. Literally viral cardigan enamel pin whatever", user_id: @employee.id, overtime_request: 2.5)
 end
 puts "20 Posts have been created"
 
--- lib/tasks/notification.rake
+++ lib/tasks/notification.rake
@@ -2,10 +2,13 @@ namespace :notification do
   desc "Sends SMS notification to employees asking them to log if they had overtime or not"
   task sms: :environment do
 	  if Time.now.wednesday?
-	  	# 1. Schedule Sundat at 5pm
-	  	# 2. Iterate over all employees
-	  	# 3. Skip AdminUsers
-	  	# 4. Send a message that has instructions and a link to log time
+	  	employees = Employee.all
+	  	notification_message = "Please login into the overtime managment dashboard
+	  	 to request overtime or confirm your hours for last week: https://dvz-overtime.herokuapp.com"
+
+	  	employees.each do |employee|
+	  		SmsTool.send_sms(number: employee.phone, message: notification_message)
+	  	end
 	  end 
   end
 
