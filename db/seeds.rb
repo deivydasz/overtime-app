4fb7ae84b1d4bd4de0519b980e9fdd0c193e976b
ecuSinner <deivydas.ziziliauskas@gmail.com>
2016-12-28 11:09:45 +0200

    Updated seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,6 +16,12 @@ AdminUser.create(email: "admintest@test.com",
 
 puts "1 Admin User created"
 
+	AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
+	AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 13.days))
+	AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 20.days))
+# end
+puts "3 Audit logs have been created"
+
 20.times do |post|
 	Post.create!(date: Date.today, rationale: "#{post} Tote bag hot chicken echo park irony mumblecore blue bottle
 	 raw denim prism slow-carb. Pinterest ennui trust fund drinking vinegar, normcore tumeric whatever jianbing kogi 
@@ -24,11 +30,5 @@ puts "1 Admin User created"
 end
 puts "20 Posts have been created"
 
-# 18.times do |audit_log|
-	AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
-	AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 13.days))
-	AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 20.days))
-# end
-puts "3 Audit logs have been created"
 
 
